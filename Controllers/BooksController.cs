c454030c0258890da20333dab30c67ca9a6d05f1
Leo Ng <leo.ng@softwire.com>
2022-09-23 16:07:07 +0100

    added delete function

--- Controllers/BooksController.cs
+++ Controllers/BooksController.cs
@@ -22,4 +22,48 @@ public class BookController : Controller
 
         return View(books);
     }
+    // get book
+    public IActionResult AddBook()
+    {
+       return View();
+    }
+    // post book
+    [HttpPost]
+    public IActionResult AddBook(Book obj)
+    {
+       var context = new BookContext();
+       context.Books.Add(obj);
+       context.SaveChanges();
+       return RedirectToAction("Index");
+    }
+    //GET delete
+    public IActionResult Delete(int? id)
+    {
+       var context = new BookContext();
+       if(id == null || id == 0)
+       {
+        return NotFound();
+       }
+       var target = context.Books.Find(id);
+       if(target == null)
+       {
+        return NotFound();
+       }
+       return View(target);
+       
+    }
+    //POST delete
+
+    public IActionResult DeletePost(int? id)
+    {
+       var context = new BookContext();
+       var target = context.Books.Find(id);
+       if(target == null)
+       {
+            return NotFound();
+       }
+       context.Books.Remove(target);
+       context.SaveChanges();
+       return RedirectToAction("Index");
+    }
 }
--- Models/Book.cs
+++ Models/Book.cs
@@ -1,8 +1,13 @@
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
 namespace bookish.Models;
 
 public class Book
 {
+    [Key]
     public int Id { get; set; }
+    [DisplayName("Book Title")]
     public string Title { get; set; }
     public string Author { get; set; }
     // public string ISBN;
